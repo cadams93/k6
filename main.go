9aa75adf4cc0d5d331eef904ec060559e6b6847a
Callum Adams <callumadams230@gmail.com>
2023-06-09 12:54:29 +1000

    Update main.go
    Add pprof endpoint.

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+
 	"go.k6.io/k6/cmd"
 )
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
 	cmd.Execute()
 }
